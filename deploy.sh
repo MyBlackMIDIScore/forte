753d1f4f284cba9273c8841de5135d2b031d9fe6
MyBlackMIDIScore <mbms@xheonin.com>
2023-05-02 00:07:15 +0300

    Add legacy CPU builds

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ build=$((build+1))
 echo "Current build: $build"
 echo "build=$build" >  "build.number"
 
+export RUSTFLAGS=""
+
 # Build executables
 echo "Building Linux executable"
 cargo build -r --target x86_64-unknown-linux-gnu
@@ -18,3 +20,17 @@ cargo build -r --target x86_64-pc-windows-gnu
 mkdir bin
 cp target/x86_64-unknown-linux-gnu/release/forte bin/forte-b$build-linux-x86_64
 cp target/x86_64-pc-windows-gnu/release/forte.exe bin/forte-b$build-windows-x86_64.exe
+
+# Build legacy executables
+export RUSTFLAGS="-C target-feature=-sse4.1,-sse4.2,-sse4a"
+
+echo "Building legacy Linux executable"
+cargo build -r --target x86_64-unknown-linux-gnu
+
+echo "Building legacy Windows executable"
+cargo build -r --target x86_64-pc-windows-gnu
+
+# Copy and rename files
+mkdir bin
+cp target/x86_64-unknown-linux-gnu/release/forte bin/forte-b$build-linux-x86_64-legacy
+cp target/x86_64-pc-windows-gnu/release/forte.exe bin/forte-b$build-windows-x86_64-legacy.exe
